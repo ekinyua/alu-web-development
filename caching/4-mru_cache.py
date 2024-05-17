c2c9770dc6abb89faba165738040f66fa1d5d492
Vulgence <e.kinyua@alustudent.com>
2024-05-17 12:33:39 +0300

    MRU

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+    BaseCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """Define MRUCache"""
+    
+    def __init__(self):
+        """Initialize MRUCache"""
+        self.stack = []
+        super().__init__()
+        
+    def put(self, key, item):
+        """assign item to the dict"""
+        if key and item:
+            if self.cache_data.get(key):
+                self.stack.remove(key)
+            while len(self.stack) >= self.MAX_ITEMS:
+                delete = self.stack.pop()
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
+            self.stack.append(key)
+            self.cache_data[key] = item
+            
+    def get(self, key):
+        """return value associated with key"""
+        valuecache = self.cache_data.get(key)
+
+        if valuecache:
+            self.queue.remove(key)
+            self.queue.append(key)
+        return valuecache
