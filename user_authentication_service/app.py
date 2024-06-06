a30a402a1a6adfc6d8499e93f0190bddd6cc7921
Vulgence <e.kinyua@alustudent.com>
2024-06-06 18:52:26 +0300

    flask app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -51,14 +51,15 @@ def login():
 
 @app.route('/sessions', methods=['DELETE'], strict_slashes=False)
 def logout():
-    """DELETE /sessions
+    """logout
     """
-    user_cookie = request.cookies.get("session_id", None)
-    user = AUTH.get_user_from_session_id(user_cookie)
-    if user_cookie is None or user is None:
-        abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect('/')
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect('/')
+    abort(403)
 
 
 @app.route('/profile', methods=['GET'], strict_slashes=False)
